9d9cced768607be4a4656f672766a6085c6d6272
Aadi <aadiprashar12345@gmail.com>
2026-05-07 17:52:17 +1000

    Created donut array with placeholder info in data js

---
+++ js/data.js
@@ -0,0 +1,110 @@
+// Placeholder info for now to test
+let temp_calories = 380;
+let temp_protein = "5g";
+let temp_carbs = "52g (Sugars: 28g)";
+let temp_fat = "18g (Saturated: 9g)";
+let temp_sodium = "210mg";
+let temp_ingredients = "Wheat flour, sugar, eggs, butter, vegetable oil, yeast, milk, salt, cinnamon, vanilla extract, and Lotus Biscoff spread (caramelised biscuits 38%, vegetable oils, sugar, soy flour, leavening agents, salt, cinnamon).";
+let temp_contains = "Gluten (wheat), dairy, eggs, soy. May contain traces of: Nuts (including peanuts and tree nuts), sesame.";
+
+// Donuts object array
+const donuts = [
+    {
+        id: 1,
+        name: "Biscoff with Caramel Drizzle",
+        price: 4.50,
+        image: "images/biscoff.png",
+        category: "popular",
+        calories: temp_calories,
+        protein: temp_protein,
+        carbs: temp_carbs,
+        fat: temp_fat,
+        sodium: temp_sodium,
+        ingredients: temp_ingredients,
+        contains: temp_contains
+    },
+    {
+        id: 2,
+        name: "Pistachio with Crushed Nuts",
+        price: 4.50,
+        image: "images/pistachio.png",
+        category: "popular",
+        calories: temp_calories,
+        protein: temp_protein,
+        carbs: temp_carbs,
+        fat: temp_fat,
+        sodium: temp_sodium,
+        ingredients: temp_ingredients,
+        contains: temp_contains
+    },
+    {
+        id: 3,
+        name: "Original Glaze",
+        price: 4.50,
+        image: "images/original-glaze.png",
+        category: "popular",
+        calories: temp_calories,
+        protein: temp_protein,
+        carbs: temp_carbs,
+        fat: temp_fat,
+        sodium: temp_sodium,
+        ingredients: temp_ingredients,
+        contains: temp_contains
+    },
+    {
+        id: 4,
+        name: "Hojicha Tea Glaze",
+        price: 4.50,
+        image: "images/tea.png",
+        category: "new",
+        calories: temp_calories,
+        protein: temp_protein,
+        carbs: temp_carbs,
+        fat: temp_fat,
+        sodium: temp_sodium,
+        ingredients: temp_ingredients,
+        contains: temp_contains
+    },
+    {
+        id: 5,
+        name: "Matcha Glaze",
+        price: 4.50,
+        image: "images/matcha.png",
+        category: "popular",
+        calories: temp_calories,
+        protein: temp_protein,
+        carbs: temp_carbs,
+        fat: temp_fat,
+        sodium: temp_sodium,
+        ingredients: temp_ingredients,
+        contains: temp_contains
+    },
+    {
+        id: 6,
+        name: "Strawberry with Vanilla",
+        price: 4.50,
+        image: "images/strawberry.png",
+        category: "best-sellers",
+        calories: temp_calories,
+        protein: temp_protein,
+        carbs: temp_carbs,
+        fat: temp_fat,
+        sodium: temp_sodium,
+        ingredients: temp_ingredients,
+        contains: temp_contains
+    },
+    {
+        id: 7,
+        name: "Dark Chocolate with Sprinkles",
+        price: 4.50,
+        image: "images/chocolate.png",
+        category: "best-sellers",
+        calories: temp_calories,
+        protein: temp_protein,
+        carbs: temp_carbs,
+        fat: temp_fat,
+        sodium: temp_sodium,
+        ingredients: temp_ingredients,
+        contains: temp_contains
+    }
+];
